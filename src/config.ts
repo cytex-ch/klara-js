667a0bd00cc40e7fd0155082555536c58adb7e62
Simon Jutz <simon.jutz@cytex.ch>
2023-08-30 17:32:38 +0200

    fix: secret env names

--- src/config.ts
+++ src/config.ts
@@ -1,6 +1,12 @@
 import 'dotenv/config';
 
-if (!(process.env.TENANT_ID && process.env.USERNAME && process.env.PASSWORD)) {
+if (
+  !(
+    process.env.KLARA_TENANT_ID &&
+    process.env.KLARA_USERNAME &&
+    process.env.KLARA_PASSWORD
+  )
+) {
   throw new Error('Missing environment variables');
 }
 
